2f0300e6daf4dc17d4ba559a298179fd8d2b9949
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2020-04-05 17:45:53 +0700

    menambahkan fitur memasukkan pasien lain (80%)

--- tubes.c
+++ tubes.c
@@ -10,11 +10,11 @@
 #include <string.h>
 #include <windows.h>
 #include <conio.h>
+#include <stdlib.h>
 
-// Waktu Dihabiskan : 12
+// Waktu Dihabiskan : 17
 
 #define DATABASEFILE "DATABASE.txt"
-#define MAX 256
 
 /* 
 * 	fungsi gotoxy dari internet 
@@ -84,22 +84,78 @@ void daftarDataString(char data[], int tab){
 
 void clearScreenInput(){
 	system("cls");
-	gotoxy(3,1); 
+	gotoxy(5,1); 
 	printf("TEKAN TOMBOL ESC UNTUK MEMBATALKAN");
+}
+
+void clearLine(WORD xLine, WORD yLine){
+	gotoxy(xLine, yLine);
+	while(xLine < 80){
+		printf(" ");
+		xLine++;
+	}
+	printf("\n");
+	gotoxy(xLine, yLine);
+}
+
+void printxy(char text[], const WORD x, const WORD y){
+	gotoxy(x,y);
+	printf("%s", text);
 }
 
 const char* inputDataString(int maxSize){
 	char* input = (char*)malloc(maxSize * sizeof(char));
 	char demo;
 	strcpy(input, " ");
-	int i=0;
+	int i = 0;
 
-	// algoritma input data
+	// algoritma input data string(alphabet + angka + simbol)
+
 	while(1){
 		demo = getch();
 
 		if(demo == 27){
-			strcpy(input, "");
+			strcpy(input, "\b");
+			break;
+		}
+
+		if(demo == '\r'){
+			break;
+		}
+
+		if(demo == '\b' && i > 0){
+			input[i-1] = '\0';
+			printf("\b \b");
+			i--;
+		}else if(demo != '\b'){
+			if(i < maxSize){
+				input[i] = demo;
+				printf("%c", input[i]);
+				i++;
+			}
+		}
+	}
+
+	if(i != strlen(input) && strcmp(input, "\b") != 0){
+		for(int j=i; j<=strlen(input); j++){
+			input[j] = '\0';
+		}
+	}
+	return input;
+}
+
+const char* inputDataInt(int maxSize){
+	char* input = (char*)malloc(maxSize * sizeof(char));
+	char demo;
+	strcpy(input, " ");
+	int i=0;
+
+	// algoritma input data (hanya integer)
+	while(1){
+		demo = getch();
+
+		if(demo == 27){
+			return "99999";
 			break;
 		}
 
@@ -112,7 +168,7 @@ const char* inputDataString(int maxSize){
 			printf("\b \b");
 			i--;
 		}else{
-			if(i < maxSize){
+			if(i < maxSize && (demo >= 48 && demo <= 57)){
 				input[i] = demo;
 				printf("%c", input[i]);
 				i++;
@@ -120,21 +176,17 @@ const char* inputDataString(int maxSize){
 		}
 	}
 
-	if(i != strlen(input)){
+	if(i != strlen(input) && strcmp(input, "\b") != 0){
 		for(int j=i; j<=strlen(input); j++){
 			input[j] = '\0';
 		}
 	}
+
 	return input;
 }
 
-void inputDataInt(){
-
-}
-
 //	deklarasi struktur data pasien
-struct data
-{
+struct data{
     int nomor,umur,status;
     char nama[50], tanggalLahir[8], penyakit[50];
 };
@@ -146,7 +198,7 @@ int main(){
     
     char data[20];
     char nama[50], tanggalLahir[8], penyakit[50]; 
-    int nomor, umur, status;
+    int nomor, umur, status, valid=0;
 
     if((db = fopen(DATABASEFILE,"r+")) == NULL){
         printf("DATABASE Tidak bisa diakses!\nMohon periksa perizinan file");
@@ -180,7 +232,7 @@ int main(){
         i++;
     }		
    
-//! menu utama
+	//! menu utama
   	int menu;
    
    	printf("Selamat Datang\n1. Tampilkan Data Pasien\n2. Tambahkan Data Pasien\n3. Ubah Data Pasien\n");
@@ -245,25 +297,182 @@ int main(){
 
 	//! algoritma untuk menambahkan data pasien
     case 2:
-	// untuk memasukkan nama pasien
+	//! untuk memasukkan nama pasien
 		clearScreenInput();
-				
 		gotoxy(5,2);
-		printf("Periksa Nama Pasien dengan teliti");
-
-		gotoxy(7,4);
-		// 
-		printf("Nama :"); strcpy(nama, inputDataString(50));
-		printf("nama yang anda tulis = %s", nama);
-	// untuk memasukkan umur pasien
-		// clearScreenInput();
 				
-		// gotoxy(5,2);
-		// printf("Periksa Nama Pasien dengan teliti");
+		while(valid == 0){
+			printf("Periksa Nama Pasien dengan teliti (hanya huruf)");
+			strcpy(nama, "");
+			clearLine(5,5);
+			gotoxy(5,5);
 
-		// gotoxy(7,4);
-		// printf("Nama :"); fflush(stdin); gets(nama);
-		// goto
+			valid = 1;
+
+			printf("Nama : "); strcpy(nama, inputDataString(50));
+			if(strcmp(nama, "\b") == 0){
+				printf("anda keluar");
+				goto keluar;
+			}
+
+		//* proses validasi nama
+			if(strlen(nama)>0){
+				for(int i=0; i<strlen(nama); i++){
+					if(nama[i] <65 || (nama[i] > 90 && nama[i] < 97) || nama[i] > 122){
+						gotoxy(5,2);
+						printf("nama tidak valid! ");
+						valid = 0;
+						break;
+					}
+				}
+			}else{
+				gotoxy(5,2);
+				printf("nama tidak valid! ");
+				valid = 0;
+			}
+		}
+
+	//! untuk memasukkan umur pasien
+		clearLine(5,2);
+		gotoxy(5,2);
+		printf("Tuliskan umur pasien dengan benar");
+
+		gotoxy(5,6);
+
+		printf("Umur : "); umur = atoi(inputDataInt(3));
+
+		if(umur == 99999){
+			printf("anda keluar");
+			goto keluar;
+		}
+
+	//! untuk memasukkan tanggal Lahir pasien
+		clearLine(1,2);
+		valid = 0;
+		gotoxy(5,2);
+
+		//* proses validasi tanggal lahir
+		while(valid == 0){
+			printf("Tuliskan Tanggal Lahir Pasien dengan benar\n");
+			gotoxy(5,3);
+			printf("Format penulisan : ddmmyyyy (8 digit)");
+
+			valid = 1;
+
+			clearLine(5, 7);
+			gotoxy(5,7);
+			printf("Tanggal Lahir : ");
+			strcpy(tanggalLahir, inputDataInt(8));
+			
+
+			if(strcmp(tanggalLahir, "99999") == 0){
+				goto keluar;
+			}
+
+			//* algoritma validasi tanggal lahir
+			if(strlen(tanggalLahir) != 8){
+				gotoxy(5,2);
+				printf("Penulisan tanggal lahir tidak valid! ");
+				valid = 0;
+			}else{
+				// mengubah masing masing tanggal lahir menjadi integer
+				int dd = (tanggalLahir[0] - '0')*10 + (tanggalLahir[1] - '0');
+				int mm = (tanggalLahir[2] - '0')*10 + (tanggalLahir[3] - '0');
+				int yyyy = (tanggalLahir[4] - '0')*1000 + (tanggalLahir[5] - '0')*100 + (tanggalLahir[6] - '0')*10 + (tanggalLahir[7] - '0');
+
+				// algoritma check tanggal lahir (termasuk kabisat)
+
+				if(yyyy < 1900 || yyyy > 2020){
+					gotoxy(5,2);
+					printf("Penulisan tanggal lahir tidak valid! ");
+					valid = 0;
+				}
+
+				switch(mm){
+				case 1:
+				case 3:
+				case 5:
+				case 7:
+				case 8:
+				case 10:
+				case 12:
+					if(dd < 1 || dd > 31){
+						gotoxy(5,2);
+						printf("Penulisan tanggal lahir tidak valid! ");
+						valid = 0;
+					}
+					break;
+				case 4:
+				case 6:
+				case 9:
+				case 11:
+					if(dd < 1 || dd > 30){
+						gotoxy(5,2);
+						printf("Penulisan tanggal lahir tidak valid! ");
+						valid = 0;
+					}
+					break;
+				case 2:
+					if(yyyy % 400 == 0 || (yyyy % 100 != 0 && yyyy % 4 == 0)){
+						if(dd < 1 || dd > 29){
+							gotoxy(5,2);
+							printf("Penulisan tanggal lahir tidak valid! ");
+							valid = 0;
+						}
+					}
+					else{
+						if(dd < 1 || dd > 28){
+							gotoxy(5,2);
+							printf("Penulisan tanggal lahir tidak valid! ");
+							valid = 0;
+						}
+					}
+					break;
+				default:
+					gotoxy(5,2);
+					printf("Penulisan tanggal lahir tidak valid! ");
+					valid = 0;
+					break;
+				}
+			}
+		}
+
+	//! untuk memasukkan status penyakit
+		clearLine(1,3);
+		valid = 0;
+		clearLine(5,2);
+		gotoxy(5,2);
+
+
+		while(valid == 0){
+			printf("Masukkan status penyakit pasien (sesuai angka indikator)\n");
+			clearLine(14, 8);
+			printxy("Indikator :",40, 4);
+			printxy("1 : ICU / Gawat darurat", 40, 5);
+			printxy("2 : Sakit Keras", 40, 6);
+			printxy("3 : Sakit biasa", 40, 7);
+			printxy("4 : Sembuh", 40, 8);
+
+			valid = 1;
+
+			gotoxy(5,8);
+			printf("status : "); status = atoi(inputDataInt(1));
+
+			if(status == 99999){
+				goto keluar;
+			}
+
+		//* validasi status
+			if(status < 1 || status > 4){
+				gotoxy(5,2);
+				printf("Status tidak valid! ");
+				valid = 0;
+			}
+		}
+
+		clearScreenInput();
+
+			printf("\nanda menulis nama: %s\nUmur: %d\nTanggal Lahir: %s\nStatus : %d", nama, umur, tanggalLahir, status);
 
 		// fflush(stdin);
         // printf("Masukkan Nama : "); gets(nama);
@@ -294,5 +503,6 @@ int main(){
     keluar:
     printf("\nanda keluar");
     fclose(db);
+	free(input);
     return 0;        
 }
